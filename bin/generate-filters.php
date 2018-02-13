a1e7d3f03d402423e997593d2351ad5e4e2a6da9
Oliver Davies <opdavies@gmail.com>
2018-02-13 02:38:29 +0000

    Update autoload paths

--- bin/generate-filters.php
+++ bin/generate-filters.php
@@ -4,7 +4,12 @@ use Opdavies\GmailFilterBuilder\Console\Command\GenerateCommand;
 use Opdavies\GmailFilterBuilder\Container\Container;
 use Symfony\Component\Console\Application;
 
-require_once __DIR__.'/../vendor/autoload.php';
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require_once __DIR__.'/../vendor/autoload.php';
+}
+elseif (file_exists(__DIR__.'/../../../autoload.php')) {
+    require_once __DIR__.'/../../../autoload.php';
+}
 
 $container = new Container();
 
