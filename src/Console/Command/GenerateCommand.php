77b83edfaebe597cb02fc1401445d94ce52e7391
Oliver Davies <oliver@oliverdavies.uk>
2019-04-11 00:09:42 +0100

    Update import

--- src/Console/Command/GenerateCommand.php
+++ src/Console/Command/GenerateCommand.php
@@ -2,7 +2,7 @@
 
 namespace Opdavies\GmailFilterBuilder\Console\Command;
 
-use Opdavies\GmailFilterBuilder\Builder;
+use Opdavies\GmailFilterBuilder\Service\Builder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
