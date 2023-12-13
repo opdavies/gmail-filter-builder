5d16b3a232d78a3cabee08b93950815f5f0a8473
Oliver Davies <oliver@oliverdavies.uk>
2023-12-13 07:02:02 +0000

    Add a Nix Flake for local development

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Gmail Filter Builder";
+
+  inputs = {
+    devshell.url = "github:numtide/devshell";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devshell.flakeModule ];
+
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devshells.default = {
+          packages = with pkgs; [ "php81" "php81Packages.composer" ];
+        };
+      };
+    };
+}
