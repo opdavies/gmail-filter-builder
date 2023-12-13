f0804b265e18408a56638e2e0b928f7e997d1ebb
Oliver Davies <oliver@oliverdavies.uk>
2023-12-13 07:36:22 +0000

    Fatal error when generating filters

--- bin/generate-filters.php
+++ bin/generate-filters.php
@@ -1,7 +1,6 @@
 <?php
 
 use Opdavies\GmailFilterBuilder\Console\Command\GenerateCommand;
-use Opdavies\GmailFilterBuilder\Container\Container;
 use Symfony\Component\Console\Application;
 
 if (file_exists(__DIR__.'/../vendor/autoload.php')) {
@@ -10,9 +9,7 @@ if (file_exists(__DIR__.'/../vendor/autoload.php')) {
     require_once __DIR__.'/../../../autoload.php';
 }
 
-$container = new Container();
-
-/** @var Application $application */
-$application = $container->get(Application::class);
+$application = new Application();
+$application->add(new GenerateCommand());
 $application->setDefaultCommand(GenerateCommand::getDefaultName());
 $application->run();
